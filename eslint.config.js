85af56e74a57bdcc7a43d8d538c48c5bfd10e612
Helder Burato Berto <helder.burato@gmail.com>
2026-03-26 16:48:53 +0000

    fix: move eslint ignores to top-level config object for global effect

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,16 @@
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, {
-  ignores: ['**/dist/**', '**/node_modules/**'],
-  rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+export default tseslint.config(
+  { ignores: ['**/dist/**', '**/node_modules/**'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
   },
-})
+)
